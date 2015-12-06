4a913145b3d2c2f0cd286d19f0ebc7fc51fe0966
unknown <andrew@elementalsystems.co.za>
2015-12-06 21:42:06 +0200

    more

--- mouse.js
+++ mouse.js
@@ -8,10 +8,22 @@ function initMouse(scene,camera,clickFunction) {
 	  function mouseDown(evt)
 	  {
 	     evt.preventDefault();
+		 uniDown(evt.clientX,evt.clientY);
+	  }
+	  
+	  
+	  function touchDown(evt)
+	  {
+	     evt.preventDefault();
+		 uniDown(evt.touches[0].clientX,evt.touches[0].clientY);
+	  }
+		 
+	  function uniDown(cx,cy)
+	  {
 	     if (dragNow) return;
 		 dragNow=true;
-		 dragStartX=evt.clientX;
-		 dragStartY=evt.clientY;		 
+		 dragStartX=cx;
+		 dragStartY=cy;		 
 		 dragStartT=tickCount();
 		 dragStartRX=scene.rotation.x;
 		 dragStartRY=scene.rotation.y;
@@ -22,24 +34,46 @@ function initMouse(scene,camera,clickFunction) {
 	  function mouseUp(evt)
 	  {
 	     evt.preventDefault();
+		 uniUp(evt.clientX,evt.clientY);
+	  }
+	  
+	  function touchUp(evt)
+	  {
+	     evt.preventDefault();
+		 uniUp(evt.changedTouches[0].clientX,evt.changedTouches[0].clientY);
+	  }
+	  
+	  function uniUp(cx,cy)
+	  {
 		 //was it a click?
-		 if ((Math.abs(evt.clientX-dragStartX)<5)&&
-		     (Math.abs(evt.clientY-dragStartY)<5)&&
+		 if ((Math.abs(cx-dragStartX)<5)&&
+		     (Math.abs(cy-dragStartY)<5)&&
 			 (Math.abs(tickCount()-dragStartT)<400)) { //I think that was a click!			 
 			 dragNow=false;
-			 clickSelect(evt.clientX,evt.clientY);			 
+			 clickSelect(cx,cy);			 
 			 return;
 		 }		 
-	     mouseMove(evt);
+	     uniMove(cx,cy);
 	     dragNow=false;
 	  }
 	  
 	  function mouseMove(evt)
 	  {
 	     evt.preventDefault();
+		 uniMove(evt.clientX,evt.clientY);
+	  }
+	  
+	  function touchMove(evt)
+	  {
+	     evt.preventDefault();
+		 uniMove(evt.touches[0].clientX,evt.touches[0].clientY);
+	  }
+	  
+	  function uniMove(cx,cy)
+	  {
 	     if (!dragNow) return;
-		 var xd=evt.clientX-dragStartX;
-		 var yd=evt.clientY-dragStartY;
+		 var xd=cx-dragStartX;
+		 var yd=cy-dragStartY;
 		 scene.rotation.x=dragStartRX+(yd/window.innerHeight)*3.14;		 
 		 scene.rotation.y=dragStartRY+(xd/window.innerWidth)*3.14;		 
 	  }
@@ -58,4 +92,8 @@ function initMouse(scene,camera,clickFunction) {
 	  document.addEventListener( 'mousemove', mouseMove, false );
       document.addEventListener( 'mousedown', mouseDown, false );
       document.addEventListener( 'mouseup', mouseUp, false );
+	  document.addEventListener( 'touchstart', touchDown, false );
+      document.addEventListener( 'touchmove', touchMove, false );
+      document.addEventListener( 'touchend', touchUp, false );
+	  
 }
